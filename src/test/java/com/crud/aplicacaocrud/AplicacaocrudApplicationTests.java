2cf05e96ff5d809c911bdcdc1e67bff18837b026
Alessandro dos Santos <alessandro_souz@outlook.com>
2024-08-24 15:07:05 -0300

    começando projeto back-end loja de roupas

---
+++ src/test/java/com/crud/aplicacaocrud/AplicacaocrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.aplicacaocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaocrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
